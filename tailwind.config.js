52e3de62bd4bb9b6d74de49ef9cf4670b9cfd6a5
BelalShah2000 <belalshah960@gmail.com>
2023-09-10 03:27:00 -0700

    project added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+//  @type {import('tailwindcss').Config} 
+module.exports = {
+  content: ["./src/index.html","./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      text:{
+        vs:"60px"
+      }
+    },
+  },
+  plugins: [],
+}
+
